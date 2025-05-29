87a5b257718ca2111cd8054c013a7d628d65b108
薛佾展 <n16418113@gmail.com>
2025-05-29 12:41:15 +0800

    初始化spring專案
    1.  spring boot 3.5.0
    2. group: com.showxd
    3. artifact: nurse-management
    4. java 17
    5. spring web
    6. spring data jpa
    7. mysql driver
    8. lombok
    9. thymeleaf

---
+++ nurse-management/src/test/java/com/showxd/nurse_management/NurseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showxd.nurse_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
